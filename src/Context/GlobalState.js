4559eb6ad0c07142cbe7d79a6dd1974af03b240c
Marvin Lewis <marvinlewis567@gmail.com>
2023-11-05 18:04:07 -0500

    The finished product

--- src/Context/GlobalState.js
+++ src/Context/GlobalState.js
@@ -18,8 +18,24 @@ export const GlobalContext = createContext(InitalState);
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(appReducer, InitalState);
 
+    //Actions
+    function deleteTransaction(id){
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
+    function addTransaction(transaction){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
+
     return (<GlobalContext.Provider value={{
-        transactions: state.transactions
+        transactions: state.transactions, 
+        deleteTransaction, addTransaction
     }}>
         {children}
     </GlobalContext.Provider>);
